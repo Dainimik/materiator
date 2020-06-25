98037f8802661df58488f7405851c6ec000a9a04
Dainius Mikalajūnas <songeex@gmail.com>
2020-06-25 16:51:40 +0300

    Add MateriatorSettingsWindow class

---
+++ Assets/_Materiator/Editor/MateriatorSettingsWindow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace Materiator
+{
+    public class MateriatorSettingsWindow : EditorWindow
+    {
+        [MenuItem("Tools/Materiator/Settings")]
+        public static void OpenWindow()
+        {
+            GetWindow<MateriatorSettingsWindow>("Materiator Settings");
+        }
+    }
+}
