f77bb19eed1964284c9d39ba38e8e368aa21e198
Dainius Mikalajūnas <songeex@gmail.com>
2020-06-25 19:19:10 +0300

    Add MateriaSetter class and its files

---
+++ Assets/_Materiator/Editor/MateriaSetterEditor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Materiator
+{
+    [CustomEditor(typeof(MateriaSetter))]
+    public class MateriaSetterEditor : MateriatorEditor
+    {
+        private MateriaSetter _materiaSetter;
+
+        private Button _reloadButton;
+
+        private void OnEnable()
+        {
+            _materiaSetter = (MateriaSetter)target;
+        }
+
+        public override VisualElement CreateInspectorGUI()
+        {
+            InitializeEditor<MateriaSetter>();
+
+            RegisterButtons();
+
+            return root;
+        }
+
+        private void RegisterButtons()
+        {
+            _reloadButton = root.Q<Button>("ReloadButton");
+            _reloadButton.clicked += Reload;
+        }
+
+        private void Reload()
+        {
+            _materiaSetter.Initialize();
+        }
+    }
+}
---
+++ Assets/_Materiator/Scripts/MateriaSetter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace Materiator
+{
+    public class MateriaSetter : MonoBehaviour
+    {
+        public Mesh Mesh;
+        public MeshFilter MeshFilter;
+        public Renderer Renderer;
+        public MeshRenderer MeshRenderer;
+        public SkinnedMeshRenderer SkinnedMeshRenderer;
+
+        public ShaderData ShaderData;
+
+        private void Awake()
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            GetMeshReferences();
+        }
+
+        public void GetMeshReferences()
+        {
+            Renderer = GetComponent<Renderer>();
+            MeshFilter = GetComponent<MeshFilter>();
+            MeshRenderer = GetComponent<MeshRenderer>();
+            SkinnedMeshRenderer = GetComponent<SkinnedMeshRenderer>();
+
+            if (MeshFilter == null)
+                Mesh = SkinnedMeshRenderer.sharedMesh;
+            else
+                Mesh = MeshFilter.sharedMesh;
+        }
+    }
+}
