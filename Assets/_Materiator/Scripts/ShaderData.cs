a477fa3d02dd72fcb71a76891374ab1256514571
Dainius Mikalajūnas <songeex@gmail.com>
2020-06-25 18:06:15 +0300

    Add ShaderData class

---
+++ Assets/_Materiator/Scripts/ShaderData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Materiator
+{
+    [CreateAssetMenu(menuName = "Materiator/Shader Data", fileName = "ShaderData")]
+    public class ShaderData : ScriptableObject
+    {
+        public Shader Shader;
+        public string BaseColorPropertyName;
+        public string MainTexturePropertyName;
+        public string MetallicSmoothnessTexturePropertyName;
+        public string SpecularGlossTexturePropertyName;
+        public string EmissionColorPropertyName;
+        public string EmissionTexturePropertyName;
+        public string MetallicSmoothnessKeywordName;
+        public string EmissionKeywordName;
+        public bool IsEditable = true;
+    }
+}
