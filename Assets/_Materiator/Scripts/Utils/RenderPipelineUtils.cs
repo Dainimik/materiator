f56b546bc0092d55e1e2a4d784a18c5035166a00
Dainius Mikalajūnas <songeex@gmail.com>
2020-06-25 16:44:52 +0300

    Add render pipeline utils class

---
+++ Assets/_Materiator/Scripts/Utils/RenderPipelineUtils.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine.Rendering;
+
+namespace Materiator
+{
+    public static class RenderPipelineUtils
+    {
+        public enum PipelineType
+        {
+            Unsupported,
+            BuiltIn,
+            Universal,
+            HD
+        }
+
+        /// <summary>
+        /// Returns the type of renderpipeline that is currently running
+        /// </summary>
+        /// <returns></returns>
+        public static PipelineType GetActivePipelineType()
+        {
+#if UNITY_2019_1_OR_NEWER
+            if (GraphicsSettings.renderPipelineAsset != null)
+            {
+                var srpType = GraphicsSettings.renderPipelineAsset.GetType().ToString();
+                if (srpType.Contains("HDRenderPipelineAsset"))
+                {
+                    return PipelineType.HD;
+                }
+                else if (srpType.Contains("UniversalRenderPipelineAsset") || srpType.Contains("LightweightRenderPipelineAsset"))
+                {
+                    return PipelineType.Universal;
+                }
+                else return PipelineType.Unsupported;
+            }
+#elif UNITY_2017_1_OR_NEWER
+            if (GraphicsSettings.renderPipelineAsset != null) {
+                return PipelineType.Unsupported;
+            }
+#endif
+            return PipelineType.BuiltIn;
+        }
+    }
+}
