09d8c555a5071822d446ba77a3012ac12ce2e1de
Dainius Mikalajūnas <songeex@gmail.com>
2020-06-25 16:46:34 +0300

    add atlas factory and utils classes

---
+++ Assets/_Materiator/Editor/Utils/AtlasFactory.cs
@@ -0,0 +1,184 @@
+﻿/*using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace Materiator
+{
+    public static class AtlasFactory
+    {
+        public static bool CheckMateriaSetterCompatibility(MateriaSetter ms)
+        {
+            if (ms.MateriaPreset != null
+                && ms.MateriaPreset.Material != null
+                && ms.IsMateriaSetterDirty == false)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public static void CreateAtlas(KeyValuePair<ShaderData, List<MateriaSetter>> group, Material material, string path, bool saveAsNewPrefabs, string newPrefabSuffix)
+        {
+            var csCount = 0;
+            List<GameObject> prefabs = new List<GameObject>();
+            List<string> prefabPaths = new List<string>();
+            foreach (var ms in group.Value)
+            {
+                if (CheckMateriaSetterCompatibility(ms))
+                {
+                    var root = ms.transform.root;
+                    var prefabGO = AssetDatabase.LoadAssetAtPath<GameObject>(AssetDatabase.GetAssetPath(root));
+                    if (!prefabs.Contains(prefabGO))
+                    {
+                        prefabs.Add(prefabGO);
+                        prefabPaths.Add(AssetDatabase.GetAssetPath(root));
+                    }
+                    csCount++;
+                }
+            }
+
+            if (saveAsNewPrefabs)
+            {
+                for (int i = 0; i < prefabs.Count; i++)
+                {
+                    var newPath = AssetUtils.GetDirectoryName(prefabPaths[i]) + "/" + AssetUtils.GetFileName(prefabPaths[i]) + newPrefabSuffix + AssetUtils.GetFileExtension(prefabPaths[i], true);
+                    AssetDatabase.CopyAsset(prefabPaths[i], newPath);
+
+                    prefabs[i] = AssetDatabase.LoadAssetAtPath<GameObject>(newPath);
+                    prefabPaths[i] = newPath;
+                }
+            }
+
+            var dir = AssetUtils.GetDirectoryName(path);
+            var atlasName = AssetUtils.GetFileName(path);
+
+            var rects = Utils.CalculateRects(csCount);
+            var rectIndex = 0;
+            var gridSize = Utils.CalculateAtlasSize(csCount);
+
+            var atlas = CreateColorAtlasAsset(dir, atlasName, material, gridSize);
+
+            var processedPrefabs = new HashSet<GameObject>();
+            var skipSavingPrefab = false;
+
+            for (var i = 0; i < prefabs.Count; i++)
+            {
+                var prefab = PrefabUtility.LoadPrefabContents(AssetDatabase.GetAssetPath(prefabs[i]));
+                var ms = prefab.GetComponentsInChildren<MateriaSetter>();
+
+                if (!processedPrefabs.Contains(prefab))
+                {
+                    for (int j = 0; j < ms.Length; j++)
+                    {
+                        var nearestPrefabInstanceRoot = PrefabUtility.GetNearestPrefabInstanceRoot(ms[j]);
+                        if (processedPrefabs.Contains(nearestPrefabInstanceRoot))
+                            continue;
+
+                        processedPrefabs.Add(nearestPrefabInstanceRoot);
+
+                        var atlasedMesh = Utils.CopyMesh(ms[j].Mesh);
+                        var remappedUVs = atlasedMesh.uv;
+
+                        for (var k = 0; k < remappedUVs.Length; k++)
+                        {
+                            var uv = remappedUVs[k];
+
+                            uv.x = rects[rectIndex].x + (uv.x * rects[rectIndex].width);
+                            uv.y = rects[rectIndex].y + (uv.y * rects[rectIndex].height);
+
+                            remappedUVs[k] = uv;
+                        }
+
+                        atlasedMesh.uv = remappedUVs;
+                        if (ms[j].MeshData == null)
+                        {
+                            var newMeshData = CreateMeshData(ms[j].Mesh.name, ms[j].Mesh, atlasedMesh, rects[rectIndex], gridSize, atlas);
+
+                            atlas.MeshDatas.Add(newMeshData);
+                            atlas.MateriaPresets.Add(ms[j].MateriaPreset);
+                            atlas.ShaderData = group.Key;
+
+                            ms[j].MeshData = newMeshData;
+                            ms[j].MateriaAtlas = atlas;
+                            ms[j].Mesh = newMeshData.AtlasedMesh;
+                            ms[j].Textures = atlas.Textures;
+                            //--------------
+                            // This is wrong and needs to be sorted out
+                            //--------------
+                            ms[j].Material = atlas.Material;
+                            ms[j].Renderer.sharedMaterial = atlas.Material;
+                            //------------
+                            ms[j].SetTextures();
+                            AssetDatabase.AddObjectToAsset(newMeshData, prefabs[i]);
+                            AssetDatabase.AddObjectToAsset(atlasedMesh, newMeshData);
+                            AssetDatabase.SaveAssets();
+                        }
+
+                        ms[j].LoadAtlas(atlas);
+                        PopulateColorAtlas(ms[j], false, atlas, ms[j].MateriaPreset, rects[rectIndex]);
+                        rectIndex++;
+                    }
+
+                    processedPrefabs.Add(prefab);
+                }
+                else
+                    skipSavingPrefab = true;
+
+                if (!skipSavingPrefab)
+                {
+                    PrefabUtility.SaveAsPrefabAsset(prefab, prefabPaths[i]);
+                    PrefabUtility.UnloadPrefabContents(prefab);
+                }
+            }
+            AssetDatabase.Refresh();
+
+            foreach (var item in prefabs)
+                AssetDatabase.ImportAsset(AssetDatabase.GetAssetPath(item));
+        }
+
+        private static MateriaAtlas CreateColorAtlasAsset(string directory, string name, Material material, int size)
+        {
+            var atlas = AssetUtils.CreateScriptableObjectAsset<MateriaAtlas>(directory, name);
+
+            atlas.Textures.CreateTextures(size, size);
+            atlas.Textures.SetNames(name);
+            atlas.Material = Object.Instantiate(material);
+
+            AssetDatabase.AddObjectToAsset(atlas.Material, atlas);
+            atlas.Textures.AddTexturesToAsset(atlas);
+
+            AssetDatabase.SaveAssets();
+
+            atlas.Textures.ImportTextureAssets();
+            atlas.MeshDatas = new List<MeshData>();
+            atlas.MateriaPresets = new List<MateriaPreset>();
+
+            return atlas;
+        }
+
+        private static void PopulateColorAtlas(MateriaSetter ms, bool copyPixels, MateriaAtlas atlas, MateriaPreset cp, Rect r)
+        {
+            if (copyPixels)
+            {
+                // not yet implemented
+            }
+            else
+                ms.Refresh();
+        }
+
+        private static MeshData CreateMeshData(string name, Mesh originalMesh, Mesh atlasedMesh, Rect uvRect, int gridSize, MateriaAtlas atlas)
+        {
+            var md = ScriptableObject.CreateInstance<MeshData>();
+
+            md.name = name;
+            md.AtlasedMesh = atlasedMesh;
+            md.OriginalMesh = originalMesh;
+            md.UVRect = uvRect;
+            md.GridSize = gridSize;
+            md.Atlas = atlas;
+
+            return md;
+        }
+    }
+}*/
---
+++ Assets/_Materiator/Scripts/Utils/Utils.cs
@@ -0,0 +1,119 @@
+﻿/*using UnityEngine;
+
+namespace Materiator
+{
+    public static class Utils
+    {
+        public static MateriatorSettings Settings = LoadSettings();
+        public static Material CreateMaterial(Shader shader, string name = null)
+        {
+            var mat = new Material(shader);
+            if (name != null)
+                mat.name = name;
+
+            mat.SetColor(Settings.DefaultShaderData.MainColorPropertyName, Color.white);
+            mat.EnableKeyword(Settings.DefaultShaderData.MetallicSmoothnessKeywordName);
+            return mat;
+        }
+
+        public static void UpdateMaterial(Material material, ShaderData shaderData)
+        {
+            material.SetColor(shaderData.MainColorPropertyName, Color.white);
+            material.EnableKeyword(shaderData.MetallicSmoothnessKeywordName);
+        }
+
+        public static void PackTextures(Texture2D[] textures, Rect[] rects, Texture2D atlasTexture)
+        {
+            var rt = new RenderTexture(atlasTexture.width, atlasTexture.height, 16, RenderTextureFormat.ARGB32, RenderTextureReadWrite.Default);
+            RenderTexture.active = rt;
+            GL.Clear(true, true, Color.black);
+            for (int i = 0; i < textures.Length; i++)
+            {
+                Graphics.Blit(textures[i], rt, new Vector2(rects[i].width, rects[i].height), new Vector2(rects[i].x, rects[i].y));
+            }
+            
+            atlasTexture.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+            RenderTexture.active = null;
+            atlasTexture.Apply(false);
+        }
+
+        public static Rect[] CalculateRects(int number)
+        {
+            Rect[] rects = new Rect[number];
+            var size = CalculateAtlasSize(number);
+
+            var sizeMultiplier = 4 / (float)size;
+
+            for (int i = 0, y = 0; y < size / 4; y++)
+            {
+                for (int x = 0; x < size / 4; x++, i++)
+                {
+                    if (i >= number) break;
+                    rects[i] = new Rect(x * sizeMultiplier, y * sizeMultiplier, sizeMultiplier, sizeMultiplier);
+                }
+            }
+
+            return rects;
+        }
+
+        public static int CalculateAtlasSize(int numberOfMeshes)
+        {
+            var ranges = new Vector2[] { new Vector2(0, 2), new Vector2(1, 5), new Vector2(4, 17), new Vector2(16, 65), new Vector2(64, 257), new Vector2(256, 1025), new Vector2(1024, 4097), new Vector2(4096, 16385), new Vector2(16384, 65537), new Vector2(65536, 262145), new Vector2(262144, 1048577), new Vector2(1048576, 4194305) };
+            var size = 4;
+            for (int i = 0; i < ranges.Length; i++)
+            {
+                if (numberOfMeshes > ranges[i].x && numberOfMeshes < ranges[i].y)
+                {
+                    size = 4 * (int)Mathf.Pow(2, i);
+                }
+            }
+            return size;
+        }
+
+        public static Mesh CopyMesh(Mesh mesh)
+        {
+            return Mesh.Instantiate(mesh);
+        }
+
+        public static void GenerateColorDataIcons(Materia materia, Material material)
+        {
+            if (materia.PreviewIcon != null)
+                Object.DestroyImmediate(materia.PreviewIcon);
+
+            if (materia.PreviewIconGray != null)
+                Object.DestroyImmediate(materia.PreviewIconGray);
+
+            material.SetColor(Settings.DefaultShaderData.MainColorPropertyName, materia.Color);
+            material.SetFloat("_Metallic", materia.Metallic);
+            material.SetFloat("_Smoothness", materia.Smoothness);
+            if (materia.IsEmissive)
+            {
+                material.EnableKeyword(Settings.DefaultShaderData.EmissionKeywordName);
+                material.globalIlluminationFlags = Settings.GlobalIlluminationFlag;
+                material.SetColor(Settings.DefaultShaderData.EmissionColorPropertyName, materia.Emission);
+            }
+            else
+            {
+                material.DisableKeyword(Settings.DefaultShaderData.EmissionKeywordName);
+                material.globalIlluminationFlags = MaterialGlobalIlluminationFlags.EmissiveIsBlack;
+            }
+
+            materia.PreviewIcon = GenerateThumbnail(material, Color.black);
+            materia.PreviewIconGray = GenerateThumbnail(material, new Color(0.78431f, 0.78431f, 0.78431f, 1));
+        }
+
+        public static Texture2D GenerateThumbnail(Material material, Color backgroundColor)
+        {
+            RuntimePreviewGenerator.BackgroundColor = backgroundColor;
+            RuntimePreviewGenerator.PreviewDirection = new Vector3(-0.50f, 0.80f, 1f);
+            RuntimePreviewGenerator.Padding = -0.20f;
+            var tex = RuntimePreviewGenerator.GenerateMaterialPreview(material, PrimitiveType.Sphere, 128, 128);
+            return tex;
+        }
+
+        public static MateriatorSettings LoadSettings()
+        {
+            return Resources.Load<MateriatorSettings>("MateriatorSettings");
+        }
+    }
+}*/
