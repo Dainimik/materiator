e6df67eb948e623a2befb74b3e2f983a6d212c80
Dainius Mikalajūnas <songeex@gmail.com>
2020-06-25 19:18:52 +0300

    Add MateriatorEditor class

---
+++ Assets/_Materiator/Editor/MateriatorEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Materiator
+{
+    public abstract class MateriatorEditor : Editor
+    {
+        protected VisualElement root;
+        protected VisualTreeAsset tree;
+
+        protected void InitializeEditor<T>()
+        {
+            root = new VisualElement();
+            tree = Resources.Load<VisualTreeAsset>(typeof(T).Name);
+            tree.CloneTree(root);
+        }
+    }
+}
