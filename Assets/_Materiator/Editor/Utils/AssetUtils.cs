c8a8f6de1586574aa1cc15cd58a62ebe26fef962
Dainius Mikalajūnas <songeex@gmail.com>
2020-06-25 16:46:17 +0300

    Add editor and asset utils classes

---
+++ Assets/_Materiator/Editor/Utils/AssetUtils.cs
@@ -0,0 +1,245 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Experimental.SceneManagement;
+using UnityEngine;
+
+namespace Materiator
+{
+    public static class AssetUtils
+    {
+        public static T CreateOrReplaceAsset<T>(T asset, string path) where T : Object
+        {
+            T existingAsset = AssetDatabase.LoadAssetAtPath<T>(path);
+
+            if (existingAsset == null)
+            {
+                AssetDatabase.CreateAsset(asset, path);
+                existingAsset = asset;
+            }
+            else
+                EditorUtility.CopySerialized(asset, existingAsset);
+
+            return existingAsset;
+        }
+
+        public static T CreateOrReplaceScriptableObjectAsset<T>(T asset, string path, out bool wasExisting) where T : ScriptableObject
+        {
+            wasExisting = false;
+            T existingAsset = AssetDatabase.LoadAssetAtPath<T>(path);
+
+            if (existingAsset == null)
+            {
+                asset = ScriptableObject.CreateInstance<T>();
+                AssetDatabase.CreateAsset(asset, path);
+                existingAsset = asset;
+                EditorUtility.SetDirty(asset);
+                AssetDatabase.ImportAsset(path);
+            }
+            else
+            {
+                EditorUtility.CopySerialized(asset, existingAsset);
+                wasExisting = true;
+            }
+
+            return existingAsset;
+        }
+
+        public static T CreateScriptableObjectAsset<T>(string directory, string name) where T : ScriptableObject
+        {
+            T asset = ScriptableObject.CreateInstance<T>();
+            AssetDatabase.CreateAsset(asset, directory + "/" + name + ".asset");
+            EditorUtility.SetDirty(asset);
+            AssetDatabase.ImportAsset(directory + "/" + name + ".asset");
+            return AssetDatabase.LoadAssetAtPath<T>(directory + "/" + name + ".asset");
+        }
+
+        public static string GetFileName(string path, bool includeExtension = false)
+        {
+            var name = "";
+            string[] splitPath = path.Split(char.Parse("/"));
+            if (includeExtension)
+            {
+                return splitPath[splitPath.Length - 1];
+            }
+            else
+            {
+                var presetNameFrag = splitPath[splitPath.Length - 1].Split(char.Parse("."));
+                return presetNameFrag[0];
+            }
+        }
+
+        public static string GetFileExtension(string filePathOrName, bool includeDot)
+        {
+            var name = "";
+            string[] splitPath = filePathOrName.Split(char.Parse("/"));
+            var presetNameFrag = splitPath[splitPath.Length - 1].Split(char.Parse("."));
+            if (includeDot)
+                return "." + presetNameFrag[1];
+            else
+                return presetNameFrag[1];
+        }
+
+        public static string GetRelativePathWithoutFilename(string path)
+        {
+            return AbsoluteToRelativePath(GetPathWithoutFilename(path));
+        }
+
+        public static string AbsoluteToRelativePath(string path)
+        {
+            if (path.StartsWith(Application.dataPath))
+                path = "Assets" + path.Substring(Application.dataPath.Length);
+            return path;
+        }
+
+        public static string GetPathWithoutFilename(string path)
+        {
+            string pathWithoutFilename = "";
+            var pathFragments = path.Split(char.Parse("/"));
+            var pathFragmentsLength = pathFragments.Length;
+
+            for (int i = 0; i < pathFragmentsLength; i++)
+            {
+                if (i < pathFragmentsLength - 2)
+                    pathWithoutFilename += pathFragments[i] + "/";
+                else
+                    pathWithoutFilename += pathFragments[i];
+            }
+            return pathWithoutFilename;
+        }
+
+        public static string GetEditorScriptDirectory(ScriptableObject so)
+        {
+            var script = MonoScript.FromScriptableObject(so);
+            var path = GetDirectoryName(AssetDatabase.GetAssetPath(script));
+            return path;
+        }
+
+        /// <summary>
+        /// Returns directory name with the path to it.
+        /// </summary>
+        /// <param name="directory">Absolute or relative directory.</param>
+        /// <returns></returns>
+        public static string GetDirectoryName(string directory)
+        {
+            return Path.GetDirectoryName(directory).Replace('\\', '/');
+        }
+
+        /// <summary>
+        /// Checks if a folder exists in a given parent directory.
+        /// </summary>
+        /// <param name="parentFolder"> Directory where to check for a folder. String must not end with a forward slash.</param>
+        /// <param name="folderInQuestion"> Folder to check for.</param>
+        public static void CheckDirAndCreate(string parentFolder, string folderInQuestion)
+        {
+            if (!Directory.Exists(parentFolder + "/" + folderInQuestion))
+                AssetDatabase.CreateFolder(parentFolder, folderInQuestion);
+        }
+
+        /// <summary>
+        /// Returns a HashSet of components from all Prefabs from a given directory.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="directory"> Relative prefab directory. If null, scans project Assets folder and deeper.</param>
+        /// <returns></returns>
+        public static HashSet<T> FindAllComponentsInPrefabs<T>(string[] searchDirectories = null) where T : MonoBehaviour
+        {
+            HashSet<T> objects = new HashSet<T>();
+
+            var prefabs = FindAssets<GameObject>(searchDirectories);
+            foreach (var prefab in prefabs)
+            {
+                var components = prefab.GetComponentsInChildren<T>();
+                foreach (var comp in components)
+                    objects.Add(comp);
+            }
+
+            return objects;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"> Type of asset to look for.</typeparam>
+        /// <param name="searchDirectories">If null, searches the whole project starting from the Assets folder.</param>
+        /// <returns></returns>
+        public static List<T> FindAssets<T>(string[] searchDirectories = null) where T : Object
+        {
+            if (searchDirectories == null)
+                searchDirectories = new string[] { "Assets" };
+
+            var assets = new List<T>();
+            var assetGUIDs = AssetDatabase.FindAssets("t:" + typeof(T).Name, searchDirectories);
+            foreach (var guid in assetGUIDs)
+                assets.Add(AssetDatabase.LoadAssetAtPath<T>(AssetDatabase.GUIDToAssetPath(guid)));
+
+            return assets;
+        }
+
+        public static T LoadAssetFromUniqueAssetPath<T>(string aAssetPath) where T : UnityEngine.Object
+        {
+            if (aAssetPath.Contains("::"))
+            {
+                string[] parts = aAssetPath.Split(new string[] { "::" }, System.StringSplitOptions.RemoveEmptyEntries);
+                aAssetPath = parts[0];
+                if (parts.Length > 1)
+                {
+                    string assetName = parts[1];
+                    System.Type t = typeof(T);
+                    var assets = AssetDatabase.LoadAllAssetsAtPath(aAssetPath)
+                        .Where(i => t.IsAssignableFrom(i.GetType())).Cast<T>();
+                    var obj = assets.Where(i => i.name == assetName).FirstOrDefault();
+                    if (obj == null)
+                    {
+                        int id;
+                        if (int.TryParse(parts[1], out id))
+                            obj = assets.Where(i => i.GetInstanceID() == id).FirstOrDefault();
+                    }
+                    if (obj != null)
+                        return obj;
+                }
+            }
+            return AssetDatabase.LoadAssetAtPath<T>(aAssetPath);
+        }
+
+        public static int GetPixelCount(Texture2D texture)
+        {
+            return texture.width * texture.height;
+        }
+
+        public static bool IsTextureReadable(Texture2D texture)
+        {
+            if (texture.isReadable)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool HasSuportedTextureFormat(Texture2D texture)
+        {
+            if (texture.format == TextureFormat.ARGB32 || texture.format == TextureFormat.RGBA32 || texture.format == TextureFormat.RGB24)
+                return true;
+            else
+                return false;
+        }
+
+        public static T[] FindObjectsOfTypeInOpenedPrefabStage<T>(out PrefabStage prefabStage)
+        {
+            var ps = PrefabStageUtility.GetCurrentPrefabStage();
+            prefabStage = ps;
+            if (ps != null)
+            {
+                var rootGameObjects = ps.scene.GetRootGameObjects();
+                T[] objs = new T[rootGameObjects.Length];
+                for (int i = 0; i < rootGameObjects.Length; i++)
+                {
+                    var obj = rootGameObjects[i].GetComponent<T>();
+                    if (obj != null) objs[i] = obj;
+                }
+                return objs;
+            }
+            return null;
+        }
+    }
+}
---
+++ Assets/_Materiator/Editor/Utils/EditorUtils.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEditor.Experimental.SceneManagement;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace Materiator
+{
+    public static class EditorUtils
+    {
+        /// <summary>
+        /// Creates a new inspector window instance and locks it to inspect the specified target
+        /// </summary>
+        public static void InspectTarget(Object target)
+        {
+            var inspectorType = typeof(Editor).Assembly.GetType("UnityEditor.InspectorWindow");
+            var inspectorInstance = ScriptableObject.CreateInstance(inspectorType) as EditorWindow;
+            inspectorInstance.Show();
+            var prevSelection = Selection.activeGameObject;
+            Selection.activeObject = target;
+            var isLocked = inspectorType.GetProperty("isLocked", BindingFlags.Instance | BindingFlags.Public);
+            isLocked.GetSetMethod().Invoke(inspectorInstance, new object[] { true });
+            Selection.activeGameObject = prevSelection;
+        }
+
+        public static void DrawMessages(List<string> messages, MessageType messageType)
+        {
+            for (int i = 0; i < messages.Count; i++)
+                EditorGUILayout.HelpBox(messages[i], messageType);
+        }
+
+        public static int ShowObjectPicker<T>(Object obj = null, bool allowSceneObjects = false, string searchFilter = null) where T : Object
+        {
+            var currentPickerWindowID = EditorGUIUtility.GetControlID(FocusType.Passive) + 100;
+            EditorGUIUtility.ShowObjectPicker<T>(obj, allowSceneObjects, searchFilter, currentPickerWindowID);
+            return currentPickerWindowID;
+        }
+
+        public delegate void DrawDropAreaAction<T>(List<T> obj);
+        public static ICollection<T> DrawDropArea<T>(Rect dropAreaRect, DrawDropAreaAction<T> Action) where T : Object
+        {
+            var objs = new List<T>();
+            Event evt = Event.current;
+            switch (evt.type)
+            {
+                case EventType.DragUpdated:
+                case EventType.DragPerform:
+                    if (!dropAreaRect.Contains(evt.mousePosition))
+                        return null;
+
+                    DragAndDrop.visualMode = DragAndDropVisualMode.Copy;
+
+                    if (evt.type == EventType.DragPerform)
+                    {
+                        DragAndDrop.AcceptDrag();
+
+                        foreach (Object draggedObject in DragAndDrop.objectReferences)
+                            if (draggedObject.GetType() == typeof(T))
+                                objs.Add((T)draggedObject);
+
+                        Action(objs);
+                    }
+                    break;
+            }
+            return null;
+        }
+
+        public static string DrawSearchbar(string searchString)
+        {
+            Event e = Event.current;
+            var rect = EditorGUILayout.BeginHorizontal();
+            Rect cancelButtonRect = new Rect(EditorGUIUtility.currentViewWidth - 20f, rect.y, 20f, 20f);
+
+            if (searchString != "")
+                GUI.enabled = !(e.isMouse && cancelButtonRect.Contains(e.mousePosition));
+            
+            searchString = GUILayout.TextField(searchString, GUI.skin.FindStyle("ToolbarSeachTextField"));
+            EditorGUILayout.EndHorizontal();
+            GUI.enabled = true;
+            if (searchString != "")
+            {
+                if (GUI.Button(cancelButtonRect, "X", GUI.skin.FindStyle("ToolbarSeachCancelButton")))
+                {
+                    searchString = "";
+                    GUI.FocusControl(null);
+                }
+            }
+
+            return searchString;
+        }
+
+        public static void LoseFocusButton(Rect rect)
+        {
+            if (GUI.Button(rect, "", GUIStyle.none))
+                GUI.FocusControl(null);
+        }
+
+        public static void MarkOpenPrefabSceneDirty()
+        {
+            var prefabStage = PrefabStageUtility.GetCurrentPrefabStage();
+            if (prefabStage != null)
+                EditorSceneManager.MarkSceneDirty(prefabStage.scene);
+        }
+    }
+}
