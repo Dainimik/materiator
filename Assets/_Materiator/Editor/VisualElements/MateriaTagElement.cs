867e399b6c61794b3512b28c64f7fa4b66bcfef7
Dainius Mikalajūnas <songeex@gmail.com>
2020-06-26 16:56:56 +0300

    Add MateriaTag visual element

---
+++ Assets/_Materiator/Editor/VisualElements/MateriaTagElement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MateriaTagElement : VisualElement
+{
+    public new class UxmlFactory : UxmlFactory<MateriaTagElement> { }
+
+    public MateriaTagElement()
+    {
+        var tree = Resources.Load<VisualTreeAsset>("VisualElements/MateriaTagElement");
+        var ui = tree.CloneTree();
+
+        Add(ui);
+    }
+}
