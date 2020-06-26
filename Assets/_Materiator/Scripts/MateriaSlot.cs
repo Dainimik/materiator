d0adb29205b379d6e45727ff9fc6e2955e0c648b
Dainius Mikalajūnas <songeex@gmail.com>
2020-06-26 16:53:15 +0300

    Add MateriaSlot class

---
+++ Assets/_Materiator/Scripts/MateriaSlot.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Materiator
+{
+    [Serializable]
+    public class MateriaSlot
+    {
+        public int ID;
+        public Materia Materia;
+        public int MateriaTag;
+
+        public MateriaSlot(int id)
+        {
+            ID = id;
+            Materia = Utils.Settings.DefaultMateria;
+            MateriaTag = 0;
+        }
+    }
+}
