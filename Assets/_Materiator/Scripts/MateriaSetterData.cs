9fd1950adc2d568acfb1c57a9466a09a9f8b9098
Dainius Mikalajūnas <songeex@gmail.com>
2020-06-28 14:57:26 +0300

    Expand materia setter data class

--- Assets/_Materiator/Scripts/MateriaSetterData.cs
+++ Assets/_Materiator/Scripts/MateriaSetterData.cs
@@ -1,9 +1,13 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Materiator
 {
     public class MateriaSetterData : ScriptableObject
     {
+        public List<MateriaSlot> MateriaSlots;
+        public MateriaPreset MateriaPreset;
+        public ShaderData ShaderData;
         public Material Material;
         public Textures Textures;
     }
