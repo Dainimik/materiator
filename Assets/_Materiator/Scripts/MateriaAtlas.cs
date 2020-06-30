736f4fa1efd7cbb69e78c6ca70b65c4d06611712
Dainius Mikalajūnas <songeex@gmail.com>
2020-06-30 11:34:19 +0300

    Add MateriaAtlas class

---
+++ Assets/_Materiator/Scripts/MateriaAtlas.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Materiator
+{
+    public class MateriaAtlas : ScriptableObject
+    {
+        public ShaderData ShaderData;
+        public Material Material;
+        public Textures Textures = new Textures();
+        public List<MateriaSetterData> MateriaSetterDatas;
+    }
+}
