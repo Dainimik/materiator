dc798eea593a9821100541f8efbbdf3b6352a7cc
Dainius Mikalajūnas <songeex@gmail.com>
2020-06-30 11:47:34 +0300

    Add MateriatorEditorWindow class

---
+++ Assets/_Materiator/Editor/MateriatorEditorWindow.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Materiator
+{
+    public abstract class MateriatorEditorWindow : EditorWindow
+    {
+        protected VisualElement root;
+        protected VisualTreeAsset tree;
+
+        protected SerializedObject serializedObject;
+
+        protected void InitializeEditorWindow<T>()
+        {
+            root = rootVisualElement;
+
+            tree = Resources.Load<VisualTreeAsset>(typeof(T).Name);
+            root.styleSheets.Add(Resources.Load<StyleSheet>("Materiator"));
+            tree.CloneTree(root);
+
+            GetProperties();
+            BindProperties();
+            RegisterButtons();
+        }
+
+        protected virtual void GetProperties() { }
+
+        protected virtual void BindProperties() { }
+
+        protected virtual void RegisterButtons() { }
+    }
+}
