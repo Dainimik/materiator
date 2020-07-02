779b6d533dc3b884d3f90a6ca671ada3579f92cf
Dainius Mikalajūnas <songeex@gmail.com>
2020-07-02 12:38:14 +0300

    Update test assets

--- Assets/_Materiator/Scripts/Textures.cs
+++ Assets/_Materiator/Scripts/Textures.cs
@@ -168,8 +168,8 @@ namespace Materiator
 
         public void CopyPixelColors(Textures source, int sourceGridSize, Rect sourceRect, int destinationGridSize, Rect destinationRect)
         {
-            var destinationRectInt = new RectInt((int)(destinationRect.x * destinationGridSize), (int)(destinationRect.y * destinationGridSize), (int)(destinationRect.width * destinationGridSize), (int)(destinationRect.height * destinationGridSize));
             var sourceRectInt = new RectInt((int)(sourceRect.x * sourceGridSize), (int)(sourceRect.y * sourceGridSize), (int)(sourceRect.width * sourceGridSize), (int)(sourceRect.height * sourceGridSize));
+            var destinationRectInt = new RectInt((int)(destinationRect.x * destinationGridSize), (int)(destinationRect.y * destinationGridSize), (int)(destinationRect.width * destinationGridSize), (int)(destinationRect.height * destinationGridSize));
 
             var baseColors = Utils.ColorToColor32Array(source.Color.GetPixels(sourceRectInt.x, sourceRectInt.y, sourceRectInt.width, sourceRectInt.height));
             var metallicColors = Utils.ColorToColor32Array(source.MetallicSmoothness.GetPixels(sourceRectInt.x, sourceRectInt.y, sourceRectInt.width, sourceRectInt.height));
