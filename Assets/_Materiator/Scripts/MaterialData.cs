cba2fc173ebd567e4915be4f934b9be801896771
Dainius Mikalajūnas <songeex@gmail.com>
2020-07-03 22:40:41 +0300

    Add MaterialData class

---
+++ Assets/_Materiator/Scripts/MaterialData.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Materiator
+{
+    [CreateAssetMenu(menuName = "Materiator/Material Data", fileName = "MaterialData")]
+    public class MaterialData : ScriptableObject
+    {
+        public ShaderData ShaderData;
+        public Material Material;
+    }
+}
