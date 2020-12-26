f701a150e9a33e80186dfaff1cead15d606784a5
Polynook <polynook@polynook.com>
2020-12-26 20:57:16 +0200

    Add MateriaSetterSlot class

---
+++ Runtime/MateriaSetterSlot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Materiator
+{
+    [Serializable]
+    public class MateriaSetterSlot
+    {
+        public int ID;
+        public string Name;
+        public Rect Rect;
+        public SerializableDictionary<int, Vector2> UVs;
+        public MateriaTag Tag;
+
+        public Materia Materia;
+
+        public MateriaSetterSlot(int id, Rect rect, string name = "", MateriaTag tag = null)
+        {
+            ID = id;
+            Name = name;
+            Rect = rect;
+
+            if (tag != null)
+                Tag = tag;
+            else
+                Tag = SystemData.Settings.DefaultTag;
+        }
+    }
+}
