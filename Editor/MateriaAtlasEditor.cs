d65549386964397b470bd3f22a5629cd2f8f6468
Polynook <polynook@polynook.com>
2020-12-29 14:06:53 +0200

    Add texture settings options to materia atlas

--- Editor/MateriaAtlasEditor.cs
+++ Editor/MateriaAtlasEditor.cs
@@ -15,10 +15,14 @@ namespace Materiator
         private SerializedProperty _materialData;
         private SerializedProperty _material;
         private SerializedProperty _materiaSlots;
+        private SerializedProperty _format;
+        private SerializedProperty _filterMode;
 
         public IMGUIContainer IMGUIContainer;
         private ObjectField _materialDataObjectField;
         private ObjectField _materialObjectField;
+        private PropertyField _formatPropertyField;
+        private PropertyField _filterModePropertyField;
         private Button _generateAtlasButton;
 
         private void OnEnable()
@@ -56,7 +60,9 @@ namespace Materiator
         {
             _materialData = serializedObject.FindProperty("MaterialData");
             _material = serializedObject.FindProperty("Material");
-            _materiaSlots = serializedObject.FindProperty("MateriaSlots"); 
+            _materiaSlots = serializedObject.FindProperty("MateriaSlots");
+            _format = serializedObject.FindProperty("Format");
+            _filterMode = serializedObject.FindProperty("FilterMode");
         }
 
         protected override void BindProperties()
@@ -64,12 +70,21 @@ namespace Materiator
             IMGUIContainer = root.Q<IMGUIContainer>("IMGUIContainer");
 
             _generateAtlasButton = root.Q<Button>("GenerateAtlasButton");
+
             _materialDataObjectField = root.Q<ObjectField>("MaterialDataObjectField");
             _materialDataObjectField.objectType = typeof(MaterialData);
             _materialDataObjectField.BindProperty(_materialData);
+
             _materialObjectField = root.Q<ObjectField>("MaterialObjectField");
             _materialObjectField.objectType = typeof(Material);
             _materialObjectField.BindProperty(_material);
+
+            _formatPropertyField = root.Q<PropertyField>("Format");
+            _formatPropertyField.BindProperty(_format);
+
+            _filterModePropertyField = root.Q<PropertyField>("FilterMode");
+            _filterModePropertyField.BindProperty(_filterMode);
+
         }
 
         protected override void RegisterCallbacks()
